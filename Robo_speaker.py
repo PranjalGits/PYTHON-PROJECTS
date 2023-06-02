f932b6672e5021e6f695c1747875a85b58ae167b
PranjalGits <133793026+PranjalGits@users.noreply.github.com>
2023-06-02 10:53:43 -0700

    Delete Robo_speaker.py

--- Robo_speaker.py
+++
@@ -1,7 +0,0 @@
-import gtts
-import playsound
-print("This is a robo speaker created by Pranjal Thakre")
-text = input("Enter something (or type 'exit' to stop): ")
-sound = gtts.gTTS(text, lang="hi")
-sound.save("robo_speaker.mp3")
-playsound.playsound("robo_speaker.mp3")
